fef66a10ab42504a6a4c2d71e91cf82fe87fae66
Amber Rodriguez <amber.rodriguez0711@gmail.com>
2018-11-06 14:47:19 -0500

    wow, refreshing node stuffs

--- bash.js
+++ bash.js
@@ -2,12 +2,11 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  
   //we have to figure out how to access bash's pwd
   if(cmd === 'pwd'){
-    process.stdout.write(pwd);
+    process.stdout.write(process.cwd());
   } else {
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('Did you just type... ' + cmd);
   }
   
   process.stdout.write('\nprompt > ')
